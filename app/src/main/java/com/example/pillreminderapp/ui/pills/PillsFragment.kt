70b39cd453c5eeff7e12711314f6031e4101658d
Petros <markaryan.petr@mail.ru>
2025-05-21 21:09:24 +0400

    Initial menu commit

--- app/src/main/java/com/example/pillreminderapp/ui/pills/PillsFragment.kt
+++ app/src/main/java/com/example/pillreminderapp/ui/pills/PillsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.pillreminderapp.ui.notifications
+package com.example.pillreminderapp.ui.pills
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.pillreminderapp.databinding.FragmentNotificationsBinding
+import com.example.pillreminderapp.databinding.FragmentPillsBinding
 
-class NotificationsFragment : Fragment() {
+class PillsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentPillsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val pillsViewModel =
+            ViewModelProvider(this).get(PillsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentPillsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textPills
+        pillsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/pillreminderapp/ui/pills/PillsViewModel.kt
+++ app/src/main/java/com/example/pillreminderapp/ui/pills/PillsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.pillreminderapp.ui.notifications
+package com.example.pillreminderapp.ui.pills
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class PillsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is pills Fragment"
     }
     val text: LiveData<String> = _text
 }
